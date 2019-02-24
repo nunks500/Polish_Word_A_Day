6ed36f3c6cf257e1b490d2d3c10f3a9e47c9be1b
joel.dinis <joel.dinis@farfetch.com>
2019-02-24 02:56:59 +0000

    some updates

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const app = express()
 var cors = require('cors');
 var port = process.env.PORT || 8080;
 
-app.get('/api', async (request, response) => {
+app.get('/', async (request, response) => {
     console.log('Received request');
+    
+     // use it before all route definitions
+    app.use(cors({origin: 'null'}));
+    
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    app.use(cors({origin: '*'}));
+
+    // Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    response.setHeader('Access-Control-Allow-Credentials', true);
+
     const a = await apiFunctions.callToThirdPartyAPI().then(function(res) {
         console.log(res);
         return response.status(200).json(res);

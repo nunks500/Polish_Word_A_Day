131bb4f2656c77048431d1163f577e052b0c8691
joel.dinis <joel.dinis@farfetch.com>
2019-02-28 00:25:56 +0000

    trying

---
+++ client/apiFunctions.js
@@ -0,0 +1,33 @@
+var axios = require('axios');
+var parseString = require('xml2js').parseString;
+
+async function callToThirdPartyAPI(){
+    const xml2parse = await call();
+   
+    /*var word;
+    parseString(xml2parse, function (err, result) {
+        console.log(result['xml']['words'][0]['word'][0]);
+        word = result['xml']['words'][0]['word'][0];
+    });*/
+
+    const word = await new Promise(resolve => parseString(xml2parse, (err, result) => {
+        if (err) reject(err);
+        //else resolve(result['xml']['words'][0]['word'][0]);
+        else resolve(JSON.stringify(result));
+      }));
+    console.log('Sending data');
+    return word;
+}
+function call(){
+    return new Promise(resolve => {
+        axios.get('https://wotd.transparent.com/rss/pl-widget.xml',
+        {headers:
+          {'Content-Type': 'text/xml'}
+        }).then(res=>{
+          resolve(res.data);
+        }).catch(err=>{console.log('Something went wrong :/')});
+});
+
+}
+
+module.exports.callToThirdPartyAPI = callToThirdPartyAPI;
---
+++ client/index.js
@@ -0,0 +1,48 @@
+const apiFunctions = require('./apiFunctions');
+const express = require('express')
+const app = express()
+var cors = require('cors');
+var port = process.env.PORT || 8080;
+
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+app.get('/api', async (request, response) => {
+    console.log('Received request');
+    
+     // use it before all route definitions
+    app.use(cors({origin: 'null'}));
+    
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    app.use(cors({origin: '*'}));
+
+    // Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    response.setHeader('Access-Control-Allow-Credentials', true);
+
+    const a = await apiFunctions.callToThirdPartyAPI().then(function(res) {
+        console.log(res);
+        return response.status(200).json(res);
+    });
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
